0f6a07d2fdd2cbf84da753b18375c3a9294259c0
SlinkMass <theo.louis1@hotmail.com>
2025-01-22 14:58:47 +0000

    first entity completed

--- Client/index.js
+++ Client/index.js
@@ -1,15 +1,76 @@
 
+previousGenresArr = []
 NewGenre = document.getElementById("new_genre_button")
-NewGenre.addEventListener('click', async function(event){
+NewGenre.addEventListener("click", async function(event){
+    
+    if (document.getElementById("genre").innerText != "Genre Here..." && !(previousGenresArr.includes(document.getElementById("genre").innerText))){
+        AddToGenres(document.getElementById("genre").innerText)
+    }
+    //Get a new genre
     try{
-        let response = await fetch('http://127.0.0.1:5500/new_genre');
+        let response = await fetch("/new_genre");
         if (! response.ok) {
-            throw new Error(response.status)
+            throw new Error(response.status);
         }
         let body = await response.text();
-        console.log(body)
-        document.getElementById('genre').innerHTML=body;
+        document.getElementById("genre").innerHTML=body;
     } catch(e) {
         alert(e);
     }
+
+    get_songs(document.getElementById("genre").innerText)
 });
+
+
+function songs_on_page(song_data) {
+    for (let n = 1; n <= Object.keys(song_data).length; n++) {
+        document.getElementById("album"+String(n)).src = song_data["song"+String(n)]["image"]["url"]
+        document.getElementById("song"+String(n)).innerText = song_data["song"+String(n)]["name"]
+        document.getElementById("artist"+String(n)).innerText = song_data["song"+String(n)]["artist"]
+        document.getElementById("fullsong"+String(n)).addEventListener("click", function(event){
+            window.open(song_data["song"+String(n)]["link"],"_blank")
+        })
+    }
+    if (Object.keys(song_data).length < 10){
+        for (let n = 10; n > Object.keys(song_data).length; n--){
+            document.getElementById("album"+String(n)).src = ""
+            document.getElementById("song"+String(n)).innerText = ""
+            document.getElementById("artist"+String(n)).innerText = ""
+        }
+    }
+}
+
+function AddToGenres(genre) {
+    previousGenres = document.getElementById("PrevGenres")
+    let NewGenre = document.createElement("p")
+    NewGenre.innerHTML = genre
+    NewGenre.className = "previous_genres"
+    previousGenres.appendChild(NewGenre)
+    previousGenresArr.push(genre)
+    NewGenre.addEventListener("click", async function(event){
+        document.getElementById("genre").innerHTML=event.target.innerText
+        get_songs(event.target.innerText)
+    })
+}
+
+async function get_songs(genre) {
+    console.log(genre)
+    //Get songs for the genre
+    try{
+        let response = await fetch("/search_songs", {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({"genre": genre}),
+
+        })
+        if (! response.ok) {
+            throw new Error(response.status);
+        }
+        let body = await response.text();
+        songs_on_page(JSON.parse(body))
+    } catch(e) {
+        alert(e);
+    }    
+}
--- server.js
+++ server.js
@@ -1,18 +1,107 @@
 import fetch from "node-fetch";
 import express from "express";
 import fs from "fs";
+import "dotenv/config";
 
+//For spotify api
+const client_id = process.env.CLIENT_ID
+const client_secret = process.env.CLIENT_SECRET
+var token
 
 const app = express();
+app.use(express.json());
 app.use(express.static("client"));
 
+async function newGenre() {
+    //Here we must find a genre that has some search results with spotify (roughly a fifth of them do so can take some time)
+    const response = await fetch("https://binaryjazz.us/wp-json/genrenator/v1/genre/5");
+    const genres = await response.json();
+    //Here we must determine if any of these genres resolve
+    let resolved = false
+    for (let n = 0; n<genres.length; n++){
+            let genreText = {"genre": genres[n]}
+            let resp = await fetch(`https://api.spotify.com/v1/search?q=genre:${genres[n]}&type=track&limit=10`, {
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer " + token
+                }
+            })
+            let jsonResponse = await resp.json()
+            let songs = await (jsonResponse)["tracks"]["items"]
+            if (await songs.length != 0) {
+                //If so write it to the file and break from the loop
+                //Write songs to a "cache" for quick retrieval
+                let songs_response = {"genre": genres[n]}
+                songs.forEach(function(song, n) {
+                    songs_response["song"+String(n+1)] = {"name":song["name"], "artist":song["artists"][0]["name"], "image":song["album"]["images"][1], "link":song["external_urls"]["spotify"]}
+                })
+                fs.writeFileSync("./data/songs.json", JSON.stringify(songs_response));
+                resolved = true
+                break
+            }
+    }
+    if (!resolved) {
+        newGenre()
+    }
+}
+
 app.get("/new_genre", async function(req, resp){
-    const response = await fetch('https://binaryjazz.us/wp-json/genrenator/v1/genre/1');
-    const data = await response.json();
-    await resp.send(data)
-    let genreText = {"genre": data}
-    fs.writeFileSync("./data/genre.json", JSON.stringify(genreText));
+    let genre = JSON.parse(fs.readFileSync("./data/songs.json"))
+    await resp.send(genre["genre"])
+    newGenre() //finds genre for the next songs
 })
 
+app.get("/song_data", )
+
+app.post("/search_songs", async function(req, resp){
+    let genre = req.body.genre
+    let data = JSON.parse(fs.readFileSync("./data/songs.json"))
+    if (data["genre"] == genre) {
+        delete data["genre"]
+        resp.send(JSON.stringify(data))
+    } else {
+        let response = await fetch(`https://api.spotify.com/v1/search?q=genre:${genre}&type=track&limit=10`, {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        })
+        if (response.ok || mode=="cache") {
+            const Fulljson = await response.json();
+            let songs = Fulljson["tracks"]["items"]
+            let songs_response = {}
+            songs.forEach(function(song, n) {
+                songs_response["song"+String(n+1)] = {"name":song["name"], "artist":song["artists"][0]["name"], "image":song["album"]["images"][1], "link":song["external_urls"]["spotify"]}
+            })
+            resp.send(JSON.stringify(songs_response))
+        } else {
+            console.log(response.statusText);
+            throw new Error("Error code: ", response.status);
+        }
+    }
+    });
+
+async function GetToken() {
+    let response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "Authorization": "Basic " + (Buffer.from(client_id + ":" + client_secret).toString("base64")),
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: "grant_type=client_credentials",
+        json: true
+    });
+    if (response.ok) {
+        const jsonResponse = await response.json();
+        token = jsonResponse["access_token"]
+    } else {
+        console.log(response.statusText);
+        throw new Error("Error code: ", response.status);
+    }
+
+}
+
+GetToken()
+newGenre()
 app.listen(5500);
 
